e1cb94019f0688976f042e13f30ab9cf9d5ffeaa
deleb <penetra.okulo@gmail.com>
2021-02-24 20:07:35 +0900

    dto, entity(db), graphql migration complete

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,15 +1,23 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { createRestaurantDto } from "./dtos/create-restaurant.dto";
+import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
+import { RestaurantService } from "./restaurants.service";
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) { }
   @Query(returns => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
   @Mutation(returns => Boolean)
-  createRestaurant(@Args() createRestaurantDto: createRestaurantDto): boolean {
-    return true;
+  async createRestaurant(@Args("input") createRestaurantDto: CreateRestaurantDto): Promise<boolean> {
+    try {
+      await this.restaurantService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
 }
