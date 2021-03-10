d7c2433db7151fe6906b9141e078f873c5191124
Deleb <penetra.okulo@gmail.com>
2021-03-10 10:09:28 +0900

    auth start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,10 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
-import * as Joi from "joi";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,16 +12,19 @@ import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { User } from './users/entities/user.entity';
 import { JwtModule } from './jwt/jwt.module';
-import { JwtMiddleware } from "./jwt/jwt.middleware";
+import { JwtMiddleware } from './jwt/jwt.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
-      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid("dev", "prod").required(),
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod')
+          .required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -26,28 +34,32 @@ import { JwtMiddleware } from "./jwt/jwt.middleware";
       }),
     }),
     TypeOrmModule.forRoot({
-      type: "postgres",
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: process.env.NODE_ENV !== "prod",
+      synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
       entities: [User],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+      context: ({ req }) => ({ user: req['user'] }),
     }),
     UsersModule,
     CommonModule,
     JwtModule.forRoot({ privateKey: process.env.PRIVATE_KEY }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(JwtMiddleware).forRoutes({ path: "/graphql", method: RequestMethod.ALL });
+    consumer
+      .apply(JwtMiddleware)
+      .forRoutes({ path: '/graphql', method: RequestMethod.POST });
   }
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,19 +1,23 @@
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
-import { LoginInput, LoginOutput } from "./dtos/login.dto";
-import { User } from "./entities/user.entity";
-import { UserService } from "./users.service";
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { User } from './entities/user.entity';
+import { UserService } from './users.service';
 
 @Resolver(of => User)
 export class UserResolver {
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
   @Query(returns => Boolean)
   hi() {
     return true;
   }
 
   @Mutation(returns => CreateAccountOutput)
-  async createAccount(@Args("input") createAccountInput: CreateAccountInput
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
       // return this.userService.createAccount(createAccountInput);
@@ -28,7 +32,7 @@ export class UserResolver {
   }
 
   @Mutation(returns => LoginOutput)
-  async login(@Args("input") loginInput: LoginInput): Promise<LoginOutput> {
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
     try {
       return await this.userService.login(loginInput);
     } catch (error) {
@@ -40,5 +44,11 @@ export class UserResolver {
   }
 
   @Query(returns => User)
-  me() { }
+  me(@Context() context) {
+    if (!context.user) {
+      return;
+    } else {
+      return context.user;
+    }
+  }
 }
