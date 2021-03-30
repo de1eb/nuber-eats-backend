cfebd08d2824fa0066c86cb12546c0a2f0ab988c
deleb <penetra.okulo@gmail.com>
2021-03-30 16:55:03 +0900

    test mocking start

---
+++ src/users/user.service.spec.ts
@@ -0,0 +1,68 @@
+import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { getRepository, Repository } from "typeorm";
+import { JwtService } from "../jwt/jwt.service";
+import { MailService } from "../mail/mail.service";
+import { User } from "./entities/user.entity";
+import { Verification } from "./entities/verification.entity";
+import { UserService } from "./users.service";
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+describe("UserService", () => {
+  let service: UserService;
+  let userRepository: MockRepository<User>;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
+    }).compile();
+    service = module.get<UserService>(UserService);
+    userRepository = module.get(getRepositoryToken(User));
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+
+  describe("createAccount", () => {
+    it("should fail if user exists", () => { });
+  });
+  it.todo("login");
+  it.todo("userProfile");
+  it.todo("editProfile");
+  it.todo("verifyEmail");
+});
