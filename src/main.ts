7ee9c9a871c1cf787bc8e2c7a7965ced2d7acfb7
Noru V <penetra.okulo@gmail.com>
2024-03-15 19:04:44 +0900

    apply ssl to nest.js app deployed in ec2

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
+import { SSL_PATH } from './common/common.constans';
 // import { jwtMiddleWare } from './jwt/jwt.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const keyFile = fs.readFileSync(SSL_PATH + '/key.pem');
+  const certFile = fs.readFileSync(SSL_PATH + '/cert.pem');
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile
+    }
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
