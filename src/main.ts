ef69c69136e7aebe074b108a6c8603849f22ac6e
Noru V <penetra.okulo@gmail.com>
2024-03-30 03:30:43 +0900

    separate prod, dev options for cors, port, https

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,28 @@ import { AppModule } from './app.module';
 import { SSL_PATH } from './common/common.constans';
 
 async function bootstrap() {
-  const keyFile = fs.readFileSync(SSL_PATH + '/key.pem');
-  const certFile = fs.readFileSync(SSL_PATH + '/cert.pem');
-  const app = await NestFactory.create(AppModule, {
+  const keyFile = process.env.NODE_ENV === 'prod' ? fs.readFileSync(SSL_PATH + '/key.pem') : null;
+  const certFile = process.env.NODE_ENV === 'prod' ? fs.readFileSync(SSL_PATH + '/cert.pem') : null;
+  let app = await NestFactory.create(AppModule);
+
+  process.env.NODE_ENV === 'prod' ? app = await NestFactory.create(AppModule, {
     httpsOptions: {
       key: keyFile,
       cert: certFile
     }
-  });
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
+  }) : null;
+
+  const corsConfig = process.env.NODE_ENV === 'prod' ? {
     origin: ["https://nuber-eats.work"],
     methods: ['PUT']
-  })
-  await app.listen(4000);
+  } : {
+    origin: ["http://localhost:3000"],
+    methods: ['PUT']
+  }
+
+  const port = process.env.NODE_ENV === 'prod' ? 443 : 4000;
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(corsConfig)
+  await app.listen(port);
 }
 bootstrap();
